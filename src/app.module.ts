46f0a81c77f3a12f553e9b5371f442c16ae5c985
GMartins <guilherme.martinst.dev@gmail.com>
2023-06-13 10:34:40 -0300

    Conectando ao banco no app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://guilhermemartins:h3aZ83NYzy7ZQNGq@somngd01.cloud.dns.internal,somngd02.cloud.dns.internal,somngd03.cloud.dns.internal/?authMechanism=DEFAULT&authSource=admin&replicaSet=rs0&readPreference=secondaryPreferred')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
